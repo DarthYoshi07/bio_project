abf7605eed906f7edd4fa37086d219ac0ae63b02
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2023-03-15 14:45:55 -0700

    Convert Shell to Py
    Shell Scipting wouldn't work

---
+++ bio.py
@@ -0,0 +1,9 @@
+response = input("For the contents to be displayed choose either type in either 1 or 2")
+print(response)
+
+if reponse == 1:
+  print("res is: " + response)
+elif response == 2:
+  print("res is: " + response)
+else:
+  print("res is: " + response)
--- bio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "What do you want to be displayed first? Type 1 or 2"
-echo "==================================================="
-read res1
-echo $res1
-
-if 
