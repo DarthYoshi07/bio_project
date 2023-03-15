f8900bb081d632d820081e745485063f792fcf3d
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2023-03-15 14:51:03 -0700

    Update bio.py

--- bio.py
+++ bio.py
@@ -1,9 +1,18 @@
 response = input("For the contents to be displayed choose either type in either 1 or 2")
+set = false
 print(response)
 
-if reponse == 1:
-  print("res is: " + response)
-elif response == 2:
-  print("res is: " + response)
-else:
-  print("res is: " + response)
+while set == false: 
+  if reponse == 1:
+    print("res is: " + response)
+  elif response == 2:
+    print("res is: " + response)
+  else:
+    print("res is: " + response)
+  break
+  
+def photosynthesis():
+  print("func")
+  
+def c_r():
+  print("func")
