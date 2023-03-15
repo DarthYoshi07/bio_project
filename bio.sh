72cff7a8f23531c8fb54bfcd4c1da2e9e944a504
TheLinuxPirate <86678835+DarthYoshi07@users.noreply.github.com>
2023-03-15 14:31:49 -0700

    Update bio.sh

--- bio.sh
+++ bio.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo "What do you want to be displayed first?"
+echo "What do you want to be displayed first? Type 1 or 2"
+echo "======================================="
 read res1
 echo $res1
+
+if 
